60b1f30ad9ae4521da480654b1a5e21a014068bf
Chase-Klingel <klingelchase@gmail.com>
2018-05-07 06:48:35 -0700

    core business logic built

---
+++ Quizzler/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Chase Klingel on 5/5/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by Chase Klingel on 5/5/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
